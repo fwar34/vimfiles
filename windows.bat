a654b4112e7f2de0c847048a61a878b949b10ba9
fwar34 <fwar34@126.com>
2019-06-12 15:28:59 +0800

    add WSL settings

--- windows.bat
+++ windows.bat
@@ -1,5 +1,10 @@
 @echo off
 
+@rem ----------------------------------------------------------
+@rem windows subsystem linux:
+@rem https://github.com/goreliu/wsl-terminal
+@rem ----------------------------------------------------------
+
 rem install chocolatey
 if not exist %ALLUSERSPROFILE%\chocolatey\choco.exe (
 	@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
