448cce34af4bcb4b69dfc0cd4ea0cc9d3ff71bd3
fwar34 <fwar34@126.com>
2023-02-28 10:19:53 +0800

    update ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,23 @@
 # 放到 C:\Users\username\Documents\WindowsPowerShell 下面以管理员执行，powershell 别名
-function gtst {git status}
-function gtl {git pull}
-function gtp {git push}
-function gtcmsg {git commit -m}
-function gtco {git checkout}
-function gtcl {git clone}
-function gtd {git diff}
+function gtst {git status $args}
+function gtl {git pull $args}
+function gtp {git push $args}
+function gtcmsg {git commit -m $args}
+function gtco {git checkout $args}
+function gtcl {git clone $args}
+function gtd {git diff $args}
+function gta {git add $args}
+
+#New-Alias -Name gta -Value "git add"
+#New-Alias -Name gtmsg -Value gtcmsg
+#Set-Alias -Name gst -Value gtst
+#Set-Alias -Name gl -Value gtl -Force
+#Set-Alias -Name gp -Value gtp -Force
+#
+#Set-Alias -Name gcmsg -Value gtcmsg
+#Set-Alias -Name gco -Value gtco
+#Set-Alias -Name gcl -Value gtcl
+#Set-Alias -Name ga -Value gta
 
 Invoke-Expression (& { (lua $HOME\z.lua\z.lua --init powershell) -join "`n" })
 
